fe8aa9d1b36d120b7c4a093e517a8191c304c5b7
dimsssss <caruen@hanmail.net>
2022-09-08 23:56:00 +0900

    feat: order 모델링

--- order/domain/orders.js
+++ order/domain/orders.js
@@ -1,51 +1,45 @@
-const {v4: uuidv4} = require('uuid')
-
 module.exports = (sequelize, DataTypes) => {
-  const points = sequelize.define(
-    'sample',
+  const orders = sequelize.define(
+    'orders',
     {
-      sampleId: {
+      orderId: {
         type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
+        allowNull: false,
+      },
+      customerId: {
+        type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
         allowNull: false,
       },
-      placeId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
-      reviewId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
-      hasBonus: {
-        type: DataTypes.TINYINT,
-        allowNull: false,
-      },
-      point: {
-        type: DataTypes.INTEGER,
+      customerName: {
+        type: DataTypes.STRING,
         allowNull: false,
       },
       createdAt: {
         type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
         allowNull: false,
       },
       updatedAt: {
         type: DataTypes.DATE,
-        allowNull: false,
+        defaultValue: DataTypes.NOW,
         onUpdate: DataTypes.NOW,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
       },
     },
     {
       charset: 'utf8mb4',
       collate: 'utf8mb4_general_ci',
-      freezeTableName: true,
+      timestamp: false,
+      paronid: true,
     },
   )
 
-  return points
+  return orders
 }
