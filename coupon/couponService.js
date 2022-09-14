39fa97d0ae8dc372f0d50e7e63c3171cc4627ae2
dimsssss <caruen@hanmail.net>
2022-09-14 22:23:31 +0900

    feat: 쿠폰 조회 서비스

--- coupon/couponService.js
+++ coupon/couponService.js
@@ -4,6 +4,11 @@ const registerCouponType = async coupon => {
   return await couponRepository.createCoupon(coupon)
 }
 
+const getCouponHistories = async () => {
+  return await couponRepository.findAllCouponHistory()
+}
+
 module.exports = {
   registerCouponType,
+  getCouponHistories,
 }
