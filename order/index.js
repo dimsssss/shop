b07280426dae5db4ce1d7039503b74d0c5ab4201
dimsssss <caruen@hanmail.net>
2022-09-14 13:25:46 +0900

    feat: patch 라우터 등록

--- order/index.js
+++ order/index.js
@@ -4,5 +4,6 @@ const orderController = require('./orderController')
 const {queryStringValidator} = require('./validator')
 
 router.get('/', queryStringValidator, orderController.getOrderList)
+router.patch('/', queryStringValidator, orderController.shipOrder)
 
 module.exports = router
