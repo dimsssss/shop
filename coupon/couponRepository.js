513762cee0fa5af78aebac2d9c247446feeb9a43
dimsssss <caruen@hanmail.net>
2022-09-14 22:23:17 +0900

    feat: 쿠폰 조회 리포지터리

--- coupon/couponRepository.js
+++ coupon/couponRepository.js
@@ -3,14 +3,22 @@ const db = require('../bin/database')
 const createCoupon = async coupon => {
   try {
     const {couponTypes} = db
-    console.log(db)
     return await couponTypes.create(coupon, {raw: true})
   } catch (err) {
-    console.log(err)
+    throw new Error(err)
+  }
+}
+
+const findAllCouponHistory = async () => {
+  try {
+    const {couponHistories} = db
+    return await couponHistories.findAll({raw: true})
+  } catch (err) {
     throw new Error(err)
   }
 }
 
 module.exports = {
   createCoupon,
+  findAllCouponHistory,
 }
