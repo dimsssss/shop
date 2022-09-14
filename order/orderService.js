a48d51933818ffca2198be2a46e29fc9805606e8
dimsssss <caruen@hanmail.net>
2022-09-14 13:25:14 +0900

    feat: 주문 발송 서비스

--- order/orderService.js
+++ order/orderService.js
@@ -4,6 +4,16 @@ const getOrderList = async filter => {
   return await orderRepository.findAllOrder(filter)
 }
 
+const updateOrderStateToShip = async orderId => {
+  const order = await orderRepository.getOrder(orderId)
+  if (order.orderState !== '주문') {
+    throw new Error('주문 상태가 유효하지 않습니다')
+  }
+
+  return await orderRepository.updateOrderState(orderId)
+}
+
 module.exports = {
   getOrderList,
+  updateOrderStateToShip,
 }
